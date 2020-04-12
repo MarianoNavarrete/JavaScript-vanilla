1bc1dc304ae3506b7e5933e9aa1914e1affd1423
Catherine Navarrete <cathynavarrete@la-MacBook-Pro-de-Catherine.local>
2020-04-11 22:45:08 -0500

    otro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import Header from '../templates/header';
+import Header from '../templates/Header';
 import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
